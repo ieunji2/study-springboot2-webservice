3469e3d9fd65be274110ddeea143acc6f7c79ee8
LEEEUNJI <eunjilee3008@gmail.com>
2021-08-26 11:10:31 +0900

    수정/조회 기능 추가

--- src/main/java/com/ieunji/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/ieunji/book/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.ieunji.book.springboot.service.posts;
 
+import com.ieunji.book.springboot.domain.posts.Posts;
 import com.ieunji.book.springboot.domain.posts.PostsRepository;
+import com.ieunji.book.springboot.web.dto.PostsResponseDto;
 import com.ieunji.book.springboot.web.dto.PostsSaveRequestDto;
+import com.ieunji.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,5 +15,21 @@ public class PostsService {
 
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }
